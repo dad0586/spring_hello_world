9def9da13175b5e8980d8ce928413e3d5d86da66
OTABOY RAKHMONOV <120033884+dad0586@users.noreply.github.com>
2023-11-21 23:09:52 +0500

    bean

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,12 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context =new ClassPathXmlApplicationContext("spring-config.xml");
         Professor professor = (Professor) context.getBean("professor" , Professor.class);
+        Assistant assistant = (Assistant) context.getBean("assistant" , Assistant.class);
+        Lesson lesson = (Lesson) context.getBean("lesson" , Lesson.class);
+
         System.out.println(professor);
+        System.out.println(assistant);
+        System.out.println(lesson);
 
     }
 }
